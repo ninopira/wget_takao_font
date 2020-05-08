23853fd7e5446ac9a119cd02469dfc88f0b73f0e
masato.ninohira <masato.ninohira@brainpad.co.jp>
2020-05-08 14:55:36 +0900

    shellの追加

---
+++ wget_takao.sh
@@ -0,0 +1,4 @@
+wget https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoFonts_00303.01.zip
+unzip TakaoFonts_00303.01.zip
+mv TakaoFonts_00303.01/TakaoPGothic.ttf .
+rm -r TakaoFonts_00303.01/ TakaoFonts_00303.01.zip
